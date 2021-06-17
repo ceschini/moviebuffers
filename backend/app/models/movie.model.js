1006c04bf4290e35871525473a18d15cacc6c37c
ceschini <lucas.ceschini@pixforce.ai>
2021-06-17 17:53:14 -0300

    backend id fix

--- backend/app/models/movie.model.js
+++ backend/app/models/movie.model.js
@@ -1,14 +1,19 @@
 module.exports = mongoose => {
-    const Movie = mongoose.model(
-        "movie",
-        mongoose.Schema(
-            {
-                title: String,
-                description: String,
-                published: Boolean
-            },
-            { timestamps: true }
-        )
+    var schema = mongoose.Schema(
+        {
+            title: String,
+            description: String,
+            published: Boolean
+        },
+        { timestamps: true }
     );
+
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Movie = mongoose.model("movie", schema);
     return Movie;
 }
