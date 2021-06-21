33cd6b88a8157ca41dbab71c176b09974a8d1d7a
ceschini <lucas.ceschini@pixforce.ai>
2021-06-21 16:03:50 -0300

    frontend done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,51 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <router-link to="/" class="navbar-brand">MovieBuffers</router-link>
+        <a href class="navbar-brand" @click.prevent>MovieBuffers</a>
         <div class="navbar-nav mr-auto">
             <li class="nav-item">
+                <router-link to="/home" class="nav-link">
+                    <font-awesome-icon icon="home" /> Home
+                </router-link>
+            </li>
+            <li v-if="showAdminBoard" class="nav-item">
+                <router-link to="/admin" class="nav-link">Admin Board</router-link>
+            </li>
+            <li v-if="showModeratorBoard" class="nav-item">
+                <router-link to="/mod" class="nav-link">Moderator Board</router-link>
+            </li>
+            <li v-if="currentUser && (!showAdminBoard && !showModeratorBoard)" class="nav-item">
+                <router-link to="/mod" class="nav-link">User Board</router-link>
+            </li>
+            <li v-if="currentUser" class="nav-item">
                 <router-link to="/movies" class="nav-link">Movies</router-link>
             </li>
             <li class="nav-item">
-                <router-link to="/add" class="nav-link">Add</router-link>
+                <router-link v-if="showModeratorBoard" to="/add" class="nav-link">Add Movie</router-link>
+            </li>
+        </div>
+        <div v-if="!currentUser" class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <router-link to="/register" class="nav-link">
+                    <font-awesome-icon icon="user-plus" /> Sign Up
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/login" class="nav-link">
+                    <font-awesome-icon icon="sign-in-alt" /> Login
+                </router-link>
+            </li>
+        </div>
+        <div v-if="currentUser" class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <router-link to="/profile" class="nav-link">
+                    <font-awesome-icon icon="user" /> {{ currentUser.username }}
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <a href @click.prevent="logOut" class="nav-link">
+                    <font-awesome-icon icon="sign-out-alt" />LogOut
+                </a>
             </li>
         </div>
     </nav>
@@ -20,11 +58,32 @@
 <script>
 
 export default {
-  name: 'App',
-  components: {
-    
+  computed: {
+      currentUser() {
+          return this.$store.state.auth.user;
+      },
+      showAdminBoard() {
+          if (this.currentUser && this.currentUser.roles) {
+              return this.currentUser.roles.includes('ROLE_ADMIN');
+          }
+
+          return false;
+      },
+      showModeratorBoard() {
+          if (this.currentUser && this.currentUser.roles) {
+              return this.currentUser.roles.includes('ROLE_MODERATOR');
+          }
+
+          return false;
+      }
+  },
+  methods: {
+      logOut() {
+          this.$store.dispatch('auth/logout');
+          this.$router.push('/login');
+      }
   }
-}
+};
 </script>
 
 <style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,50 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import Register from './views/Register.vue';
 
 Vue.use(Router);
 
-export default new Router({
+export const router = new Router({
     mode: "history",
     routes: [
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: "/profile",
+            name: "profile",
+            // lazy-loaded
+            component: () => import("./views/Profile.vue")
+        },
+        {
+            path: "/admin",
+            name: "admin",
+            component: () => import("./views/BoardAdmin.vue")
+        },
+        {
+            path: "/mod",
+            name: "mod",
+            component: () => import("./views/BoardModerator.vue")
+        },
+        {
+            path: "/user",
+            name: "user",
+            component: () => import("./views/BoardUser.vue")
+        },
         {
             path: "/",
             alias: "/movies",
@@ -23,4 +62,19 @@ export default new Router({
             component: () => import("./components/AddMovie")
         }
     ]
+});
+
+// handles unauthorized access
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/login', '/register', '/home'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
+
+    // trying to access a restricted page + not logged in
+    // redirect to login page
+    if (authRequired && !loggedIn) {
+        next('/login');
+    } else {
+        next();
+    }
 });
